2c962671b8fafeaf5064498366f870f20c81579c
Jim Beaumont <jameskevinbeaumont@gmail.com>
2020-11-26 14:54:58 -0500

    Media queries complete for both pages - sprint-2

---
+++ sprint-2/scripts/biography.js
@@ -0,0 +1,21 @@
+// Create Array of Comment Objects
+let shows = [
+    {
+    "commentImage": "./assets/images/Michael-Lyons.jpg",
+    "commentName": "Michael Lyons",
+    "commentDate": "12/18/2018",
+    "commentText": "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of aconcert I have EVER witnessed."
+    },
+    {
+    "commentImage": "./assets/images/Gary-Wong.jpg",
+    "commentName": "Gary Wong",
+    "commentDate": "12/12/2018",
+    "commentText": "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
+    },
+    {
+    "commentImage": "./assets/images/ted-duncan.jpg",
+    "commentName": "Theodore Duncan",
+    "commentDate": "11/15/2018",
+    "commentText": "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
+    }
+]
