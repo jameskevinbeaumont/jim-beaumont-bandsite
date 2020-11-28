b967f25b74ac34d30ae8af0969abaef4ee7a1686
Jim Beaumont <jameskevinbeaumont@gmail.com>
2020-11-27 23:09:07 -0500

    Final spacing and diving deeper completed - sprint-2

--- sprint-2/scripts/biography.js
+++ sprint-2/scripts/biography.js
@@ -1,25 +1,25 @@
 // Create Array of Comment Objects
 let comments = [
     {
-    // "commentImage": "./assets/images/Michael-Lyons.jpg",
-    "commentImage": "",
-    "commentName": "Michael Lyons",
-    "commentDate": "12/18/2018",
-    "commentText": "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
+    'commentImage': '',
+    'commentName': 'Michael Lyons',
+    'commentDate': '11/17/2020 08:14:27',
+    'commentTS': '',
+    'commentText': 'They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.'
     },
     {
-    // "commentImage": "./assets/images/Gary-Wong.jpg",
-    "commentImage": "",
-    "commentName": "Gary Wong",
-    "commentDate": "12/12/2018",
-    "commentText": "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
+    'commentImage': '',
+    'commentName': 'Gary Wong',
+    'commentDate': '05/12/2020 23:24:00',
+    'commentTS': '',
+    'commentText': 'Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!'
     },
     {
-    // "commentImage": "./assets/images/ted-duncan.jpg",
-    "commentImage": "",
-    "commentName": "Theodore Duncan",
-    "commentDate": "11/15/2018",
-    "commentText": "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
+    'commentImage': '',
+    'commentName': 'Theodore Duncan',
+    'commentDate': '08/15/2018 15:48:57',
+    'commentTS': '',
+    'commentText': 'How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!'
     }
 ]
 
@@ -71,11 +71,14 @@ const displayComment = (commentObj, i) => {
     commentName.classList.add('comment__header-name');
     commentName.innerText = commentObj.commentName;
     commentHeader.appendChild(commentName);
-    // Create comment header - date
-    let commentDate = document.createElement('p');
-    commentDate.classList.add('comment__header-date');
-    commentDate.innerText = commentObj.commentDate;
-    commentHeader.appendChild(commentDate);
+    // Update the timestamp on the comment detail
+    // based on the current date and time
+    commentObj.commentTS = generateTimeStamp(commentObj.commentDate);
+    // Create comment header - timestamp (ts)
+    let commentTS = document.createElement('p');
+    commentTS.classList.add('comment__header-ts');
+    commentTS.innerText = commentObj.commentTS;
+    commentHeader.appendChild(commentTS);
     // Create comment comments
     let commentComments = document.createElement('div');
     commentComments.classList.add('comment__comments');
@@ -87,16 +90,27 @@ const displayComment = (commentObj, i) => {
     commentComments.appendChild(commentText);
 };
 
+// Function - Insert final comment divider
+const insertCommentDivider = () => {
+    let ref = document.getElementById('comment-' + (maxCommentIndex));
+    let commentDivider = document.createElement('hr');
+    commentDivider.classList.add('comment__divider');
+    insertAfter(commentDivider, ref);
+};
+
 // Function - Insert HTML after parent node
 const insertAfter = (el, referenceNode) => {
     referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
 };
 
-// Function - Iterate comment object and build HTML for comment display
+// Function - Iterate comment object array and build HTML for comment display
+let maxCommentIndex = 0;
 const displayComments = () => {
     comments.forEach(function(comment, i) {
         displayComment(comment, i);
-    });    
+        maxCommentIndex = i;
+    });
+    insertCommentDivider();
 };
 
 // Function - Format current date (mm/dd/yyyy)
@@ -105,6 +119,8 @@ const getCurrentDate = () => {
     let dd = today.getDate();
     let mm = today.getMonth() + 1;
     let yyyy = today.getFullYear();
+    let time = Math.round(today.getTime() / 1000);
+    let timeFormat = convertSecondsToTime(time);
 
     if (dd < 10) {
         dd = '0' + dd;
@@ -114,7 +130,79 @@ const getCurrentDate = () => {
         mm = '0' + mm;
     }
 
-    return mm + '/' + dd + '/' + yyyy;
+    return `${mm}/${dd}/${yyyy} ${timeFormat}`;
+};
+
+// Function - Convert seconds to HH:MM:SS
+const convertSecondsToTime = (time) => {
+    let dateObj = new Date(); 
+    let hours = dateObj.getHours(); 
+    let minutes = dateObj.getMinutes(); 
+    let seconds = dateObj.getSeconds(); 
+
+    return hours.toString().padStart(2, '0') 
+        + ':' + minutes.toString().padStart(2, '0') 
+        + ':' + seconds.toString().padStart(2, '0'); 
+};
+
+// Function - Generate timestamp
+const generateTimeStamp = (commentObjDate) => {
+    let currentDate = new Date();
+    let commentDate = new Date(commentObjDate);
+    let currentTime = Math.round(currentDate.getTime() / 1000);
+    let commentTime = Math.round(commentDate.getTime() / 1000);
+    let diffMins = (currentTime - commentTime) / 60;
+    let diffHours = parseInt(Math.round(diffMins / 60));
+    let diffDays = parseInt(Math.round(diffHours / 24));
+    let diffWeeks = parseInt(Math.round(diffDays / 7));
+    let diffMonths = parseInt(Math.round(diffWeeks / 4));
+    let diffYears = parseInt(Math.round(diffMonths / 12));
+    let tsText = '';
+
+    switch(true) {
+        case (diffMins < 1):
+            tsText = 'Posted less than 1 minute ago';
+            break;
+        case (diffMins < 2):
+            tsText = `Posted ${parseInt(diffMins)} minute ago`;
+            break;
+        case (diffMins < 60):
+            tsText = `Posted ${parseInt(diffMins)} minutes ago`;
+            break;
+        case (diffHours == 1):
+            tsText = `Posted 1 hour ago`;
+            break;
+        case (diffHours < 24):
+            tsText = `Posted ${parseInt((diffMins / 60))} hours ago`;
+            break;
+        case (diffHours < 48):
+            tsText = `Posted 1 day ago`;
+            break;
+        case (diffDays < 8):
+            tsText = `Posted ${diffDays} days ago`;
+            break;
+        case (diffWeeks < 2):
+            tsText = `Posted 1 week ago`;
+            break;
+        case (diffWeeks < 5):
+            tsText = `Posted ${diffWeeks} weeks ago`;
+            break;
+        case (diffMonths < 2):
+            tsText = `Posted 1 month ago`;
+            break;
+        case (diffMonths < 12):
+            tsText = `Posted ${diffMonths} months ago`;
+            break;
+        case (diffYears < 2):
+            tsText = `Posted 1 year ago`;
+            break;
+        case (diffYears >= 2):
+            tsText = `Posted ${diffYears} years ago`;
+            break;
+        default:
+    };
+
+    return tsText;
 };
 
 // Function - Remove all child nodes of a node
@@ -129,12 +217,15 @@ const commentsForm = document.getElementById('comments__form');
 
 // Event Listener - Event ('submit') - commentsForm
 commentsForm.addEventListener('submit', function(e) {
+    // Prevent page from re-loading
     e.preventDefault();
+
     // Get the comment data from the form and save it to the newComment object
     let newComment = {
         commentImage: '',
         commentName: e.target.fullName.value,
         commentDate: getCurrentDate(),
+        commentTS: '',
         commentText: e.target.commentText.value
     };
     // Add the newComment object to the first position in the object array
@@ -147,6 +238,8 @@ commentsForm.addEventListener('submit', function(e) {
     displayComments();
     // Clear the input fields
     commentsForm.reset();
+    // Reset the focus fullName input
+    document.getElementById('fullName').focus();
 });
 
 // If page is initialized or refreshed, get the comments from the object array
